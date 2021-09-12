94d5905f693269aa4e81701ae8e664af2b6d86a3
Rony163 <minhajulislamrony43@gmail.com>
2021-09-13 04:00:24 +0600

    Update style and set rating

--- js/app.js
+++ js/app.js
@@ -11,6 +11,7 @@ const showProducts = (products) => {
   const allProducts = products.map((pd) => pd);
   for (const product of allProducts) {
     const image = product.image;
+    const { count, rate } = product.rating;
     const div = document.createElement("div");
     div.classList.add("product");
     div.innerHTML = `<div class="single-product">
@@ -19,6 +20,8 @@ const showProducts = (products) => {
       </div>
       <h3>${product.title}</h3>
       <p>Category: ${product.category}</p>
+      <p>Rating Count: ${count}</p>
+      <p>Avarage Rating: ${rate}</p>
       <h2>Price: $ ${product.price}</h2>
       <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
       <button id="details-btn" class="btn btn-danger">Details</button></div>
